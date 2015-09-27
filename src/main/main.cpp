a72a3dd2c947f273b0b98a70b07ade808e02b888
Jason Wee <peichieh@gmail.com>
2015-09-27 15:17:59 +0800

    added lecture4 class and add its header file into project include folder.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <lecture4.h>
 
 using namespace std;
 
 int main()
 {
+    lecture4 lec4;
+    lec4.learnArray();
+
     cout << "Hello world!" << endl;
     return 0;
 }
