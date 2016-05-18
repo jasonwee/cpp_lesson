44cf36aed57fc0796f776dcb9cf92c5d2c5fda23
Jason Wee <peichieh@gmail.com>
2016-05-18 15:07:46 +0800

    learn reference in another example

---
+++ src/main/src/Reference2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() 
+{
+  int intOne;
+  int &rSomeRef = intOne;
+
+  intOne = 5;
+  std::cout << "intOne: " << intOne << "\n";
+  std::cout << "rSomeRef: " << rSomeRef << "\n";
+
+  std::cout << "&intOne: " << &intOne << "\n";
+  std::cout << "&rSomeRef: " << &rSomeRef << "\n";
+
+  return 0;
+}
