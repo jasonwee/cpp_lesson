a13ca93e3b2f5bad6128327ae5b7d6c844ca4113
Jason Wee <peichieh@gmail.com>
2016-05-20 15:58:03 +0800

    learn constant pointer to passed into function

---
+++ src/main/src/ConstPasser.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+class SimpleCat
+{
+public:
+  SimpleCat();
+  SimpleCat(SimpleCat&);
+  ~SimpleCat();
+
+  int GetAge() const { return itsAge; }
+  void SetAge(int age) { itsAge = age; }
+
+private:
+  int itsAge;
+};
+
+SimpleCat::SimpleCat()
+{
+  std::cout << "Simple Cat Constructor ...\n";
+  itsAge = 1;
+}
+
+SimpleCat::SimpleCat(SimpleCat&)
+{
+  std::cout << "Simple Cat Copy Constructor ...\n";
+}
+
+SimpleCat::~SimpleCat() 
+{
+  std::cout << "Simple Cat Destructor ...\n";
+}
+
+const SimpleCat * const FunctionTwo(const SimpleCat *const theCat);
+
+
+int main()
+{
+  std::cout << "Making a cat ...\n";
+  SimpleCat Frisky;
+  std::cout << "Frisky is ";
+  std::cout << Frisky.GetAge() << " years old\n";
+  int age = 5;
+  Frisky.SetAge(age);
+  std::cout << "Frisky is ";
+  std::cout << Frisky.GetAge() << " years old\n";
+  std::cout << "Calling FunctionTwo ...\n";
+  FunctionTwo(&Frisky);
+  std::cout << "Frisky is ";
+  std::cout << Frisky.GetAge() << " years old\n";
+  return 0;
+}
+
+// functionTwo, passes a const pointer
+
+const SimpleCat * const FunctionTwo (const SimpleCat * const theCat)
+{
+  std::cout << "Function Two. Returning ...\n";
+  std::cout << "Frisky is now " << theCat->GetAge();
+  std::cout << " years old \n";
+  // theCat->SetAge(8); const!
+  return theCat;
+}
