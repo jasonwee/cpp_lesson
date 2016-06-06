72e449600a5548f33469886aefcecdc8cc44fdc1
Jason Wee <peichieh@gmail.com>
2016-06-07 00:16:10 +0800

    initial test9

---
+++ src/main/src/hr-test9.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  int n;
+  //cin >> n;
+  string s;
+  //cin >> s;
+  int k;
+  //cin >> k;
+  for (char & c : s) {
+    //cout << c << " ";
+  }
+  // cout << n << s << k;
+  
+  int i = 5;
+  char c= 'A' - 1 + i; 
+  
+  cout << char(64);
+  cout << c;
+  return 0;
+}
+
+char shiftUpper(char s1, shift k) {
+  char c= 'A' - 1 + k; 
+
+}
+
+char shiftLower() {
+
+}
+
