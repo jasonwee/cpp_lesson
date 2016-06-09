a632945f2eac5e7ca378304e083df40b50019635
Jason Wee <peichieh@gmail.com>
2016-06-10 00:06:13 +0800

    learn array of char

---
+++ src/main/src/array1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  //char greeting[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+  char greeting[5] = {'H', 'e', 'l', 'l', 'o'};
+
+  cout << "Greeting message: ";
+  cout << greeting << endl;
+
+  return 0;
+}
