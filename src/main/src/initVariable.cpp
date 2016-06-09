435f35c1a8cdf38063c89d26b81c6050b542e5b8
Jason Wee <peichieh@gmail.com>
2016-06-09 22:41:28 +0800

    variable initial value

---
+++ src/main/src/initVariable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  // all these are default values.
+  int a = 0;
+  char b= '\0';
+  float c = 0;
+  double d = 0;
+  int *pointer  = NULL;
+
+}
