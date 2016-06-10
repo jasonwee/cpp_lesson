59e7489114e0b0b8f623cc7f3cd5227cc07c574b
Jason Wee <peichieh@gmail.com>
2016-06-10 09:08:34 +0800

    learning io stream

---
+++ src/main/src/iostream.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  char str[] = "Unable to read...";
+
+  cerr << "Error message : " << str << endl;
+
+  clog << "Error message : " << str << endl;
+}
