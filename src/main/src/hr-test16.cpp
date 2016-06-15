9160b4f3b00d08bd4af9e3745c079a6ab936b46f
Jason Wee <peichieh@gmail.com>
2016-06-15 23:08:23 +0800

    initial hackerrank test 16

---
+++ src/main/src/hr-test16.cpp
@@ -0,0 +1,13 @@
+// https://www.hackerrank.com/challenges/kaprekar-numbers?h_r=next-challenge&h_v=zen
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+  return 0;
+}
